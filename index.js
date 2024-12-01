ff24a1892e4a0290adfaaaa97cf8fd721fda8c92
Maximino Castillo <maxcastgarcia@gmail.com>
2024-12-01 00:49:22 -0600

    Add accounts endpoint to update balance of account

--- index.js
+++ index.js
@@ -74,6 +74,47 @@ app.delete('/transactions/:id', (req, res) => {
   }
 });
 
+// Function to read accounts from the JSON file
+const getAccounts = () => {
+  const data = fs.readFileSync('accounts.json');
+  return JSON.parse(data);
+};
+
+// Function to write transactions to the JSON file
+const saveAccounts = (accounts) => {
+  fs.writeFileSync('transactions.json', JSON.stringify(accounts, null, 2));
+};
+
+// Get all accounts
+app.get('/accounts', (req, res) => {
+  const accounts = getAccounts();
+  res.json(accounts);
+});
+
+// Get a single account by ID
+app.get('/accounts/:id', (req, res) => {
+  const accounts = getAccounts();
+  const account = accounts.find(t => t.id === parseInt(req.params.id));
+  if (account) {
+    res.json(account);
+  } else {
+    res.status(404).send('Account not found');
+  }
+});
+
+// Update an existing account
+app.put('/accounts/:id', (req, res) => {
+  const accounts = getAccounts();
+  const index = accounts.findIndex(t => t.id === parseInt(req.params.id));
+  if (index !== -1) {
+    accounts[index] = { id: accounts[index].id, ...req.body };
+    saveAccounts(accounts);
+    res.json(accounts[index]);
+  } else {
+    res.status(404).send('Account not found');
+  }
+});
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
